5b6205272fd6d3a9eb56be28aedeed5b2aef5391
ShaykhahAldawsari <124034904+ShaykhahAldawsari@users.noreply.github.com>
2024-05-26 07:48:49 +0300

    Update api.js
    corrected the URL, it was the local link which caused an error

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://us-central1-unibite-21494.cloudfunctions.net/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
